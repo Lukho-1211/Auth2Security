b20351b2e1eb33475f2c539bed6df24a5dcb4476
luk <aluluthospambo@gmail.com>
2024-12-20 00:43:58 +0200

    src removed

--- server.js
+++ server.js
@@ -31,6 +31,19 @@ function verifyCallBack(accessToken, refreshToken, profile, done){
 
 passport.use(new Strategy(AUTH_OPTIONS, verifyCallBack));
 
+    // Save the session to cookie
+passport.serializeUser((user, done)=>{   
+    done(null, user.id); // can save the entire user[object] or parts of the session
+});
+
+    // Read the session to cookie
+passport.deserializeUser((id, done)=>{ // can also save the id here to the database 
+    // User.findbyId(id).then(user =>{
+    //     done(null, user);
+    // })
+    done(null, id);
+});
+
 const app = express();
 
 app.use(helmet());
@@ -43,8 +56,12 @@ app.use(cookieSession({ //Creation of a cookie
 
 app.use(passport.initialize());
 
+// responsible to call [ deserializeUser] 
+app.use(passport.session()); // authenticates the session with [COOKIE_KEY_1] and set req.user
+
 function checkLogedIn(req, res, next){
-    const isLogIn = true;
+    console.log('current user Id:', req.user);
+    const isLogIn = req.user;
     if(!isLogIn){
         return res.status(401).json({
             error: 'You must log in'
@@ -70,17 +87,20 @@ app.get('/auth/google/callback',
     passport.authenticate('google',{
         failureRedirect: '/failure',
         successRedirect: '/',
-        session: false,
+        session: true,
     }), 
     (req, res)=>{
         console.log('Google called us Back!!!');
     }
 );
 
-app.get('/google/logout',(req, res, next)=>{});
+app.get('/auth/logout',(req, res, next)=>{
+    req.logout(); // removes req.user and clears any log in session
+    return res.redirect('/');
+});
 
 app.get('/secret', checkLogedIn, (req,res)=>{
-    res.send('my secret');
+    res.send('my secret value is 12');
 });
 
 app.get('/failure', (req, res)=>{
